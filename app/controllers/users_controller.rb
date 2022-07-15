edad3bdc13b5bbfd1c09830f2a74b6b846412099
kotarokatsunuma <katsunuma1113@gmail.com>
2022-07-15 10:06:49 +0900

    ログイン処理の記入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,7 @@ class UsersController < ApplicationController
     @user = User.find_by(email: params[:email],
                           password: params[:password])
     if @user
+      session[:user_id] = @user.id
       flash[:notice] = "ログインしました"
       redirect_to("/posts/index")
     else
